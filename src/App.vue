ae5eedf342a9057882f07b17d22649de97836694
yanbaorui <yanbaorui1997@163.com>
2021-06-02 07:03:23 +0800

    添加qiankun框架，添加测试页面

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <el-menu :router="true" mode="horizontal">
+<el-menu-item index="/">Home</el-menu-item>
+
+<el-menu-item index="/vue1">应用1</el-menu-item>
+<el-menu-item index="/vue2">应用2</el-menu-item>
+
+    </el-menu>
+
+<!-- <router-view>
+  <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+</router-view> -->
+    <div id="vue1"></div>
+    <div id="vue2"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,25 @@ import App from './App.vue';
 
 Vue.use(ElementUI);
 
+import { registerMicroApps, start } from 'qiankun';
+
+registerMicroApps([
+  {
+    name: 'vue app1',
+    entry: '//localhost:8081',
+    container: '#vue1',
+    activeRule: '/vue2',
+  },
+  {
+    name: 'vue app2',
+    entry: '//localhost:8082',
+    container: '#vue2',
+    activeRule: '/vue2',
+  },
+]);
+
+start();
+
 new Vue({
   el: '#app',
   render: h => h(App)

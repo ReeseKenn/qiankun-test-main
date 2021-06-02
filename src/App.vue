d14e1cc795751d9c59430d9ed7249aa45276235b
reesekenn <yanbaorui1997@163.com>
2021-06-02 16:41:51 +0800

    调整

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="navbar">
+    <router-link to="/home" class="text-logo" style="float: left">
+      <h3 class="title">ALLINONE</h3>
+    </router-link>
     <el-menu :router="true" mode="horizontal">
-<el-menu-item index="/">Home</el-menu-item>
+      <el-menu-item index="/">Home</el-menu-item>
 
-<el-menu-item index="/vue1">应用1</el-menu-item>
-<el-menu-item index="/vue2">应用2</el-menu-item>
-
+      <el-menu-item index="/vue1">应用1</el-menu-item>
+      <el-menu-item index="/vue2">应用2</el-menu-item>
     </el-menu>
 
-<!-- <router-view>
-  <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-</router-view> -->
+    <router-view>
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </router-view>
     <div id="vue1"></div>
     <div id="vue2"></div>
   </div>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
-}
-</script>
 
 <style>
 #app {
@@ -36,6 +26,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,32 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App.vue";
+import router from "./router";
 
 Vue.use(ElementUI);
 
-import { registerMicroApps, start } from 'qiankun';
+import { registerMicroApps, start } from "qiankun";
 
 registerMicroApps([
   {
-    name: 'vue app1',
-    entry: '//localhost:8081',
-    container: '#vue1',
-    activeRule: '/vue2',
+    name: "vue app1",
+    entry: "//localhost:8081",
+    container: "#vue1",
+    activeRule: "/vue1",
   },
   {
-    name: 'vue app2',
-    entry: '//localhost:8082',
-    container: '#vue2',
-    activeRule: '/vue2',
+    name: "vue app2",
+    entry: "//localhost:9528",
+    container: "#vue2",
+    activeRule: "/vue2",
   },
 ]);
 
 start();
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: "#app",
+  router,
+  render: (h) => h(App),
 });
